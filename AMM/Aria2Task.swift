1c9af79ef1a42bba26a10b026ffc055aa73d99e0
15cm <sinkerine@gmail.com>
2017-02-01 00:21:06 +0800

    [+] Init [+] KVO bindings

---
+++ AMM/Aria2Task.swift
@@ -0,0 +1,70 @@
+//
+//  Aria2Task.swift
+//  AMM
+//
+//  Created by Sinkerine on 26/01/2017.
+//  Copyright © 2017 sinkerine. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Aria2Task: NSObject{
+    dynamic var gid: String = ""
+    var status: Aria2TaskStatus = .unknown {
+        didSet {
+            self.statusRawValue = self.status.rawValue
+        }
+    }
+    // Workaround for enum KVO
+    dynamic var statusRawValue: String = "unknown"
+    dynamic var title: String = ""
+    dynamic var downloadSpeed: Int = 0
+    dynamic var uploadSpeed: Int = 0
+    dynamic var totalLength: Int = 0
+    dynamic var completedLength: Int = 0
+    var files: [Aria2File] = []
+    
+    struct Aria2File {
+        var title: String
+        var size: Int
+        
+        init(title: String, size: Int) {
+            self.title = title
+            self.size = size
+        }
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    // init with task json
+    init(json: JSON) {
+        gid = json["gid"].stringValue
+        status = Aria2TaskStatus(rawValue: json["status"].stringValue) ?? .unknown
+        if let taskName = json["bittorrent"]["info"]["name"].string {
+            title = taskName
+        } else {
+            title = json["files"][0]["path"].stringValue.components(separatedBy: "/").last!
+        }
+        downloadSpeed = Int(json["downloadSpeed"].stringValue) ?? 0
+        uploadSpeed = Int(json["uploadSpeed"].stringValue) ?? 0
+        totalLength = Int(json["totalLength"].stringValue) ?? 0
+        completedLength = Int(json["completedLength"].stringValue) ?? 0
+        for (_, fileJSON) in json["files"] {
+            files.append(Aria2File(title: fileJSON["path"].stringValue, size: Int(fileJSON["length"].stringValue) ?? 0))
+        }
+    }
+    
+    func copy(fromTask task: Aria2Task) {
+        gid = task.gid
+        status = task.status
+        title = task.title
+        downloadSpeed = task.downloadSpeed
+        uploadSpeed = task.uploadSpeed
+        totalLength = task.totalLength
+        completedLength = task.completedLength
+        files = task.files
+    }
+}
