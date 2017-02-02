1b42f01fbc3830ecdccf0d648ca0714d92b6be08
15cm <sinkerine@gmail.com>
2017-02-03 00:26:21 +0800

    [+] Init

---
+++ AMM/TaskMenuItemSeperatorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TaskMenuItemSeperatorViewController.swift
+//  AMM
+//
+//  Created by Sinkerine on 02/02/2017.
+//  Copyright © 2017 sinkerine. All rights reserved.
+//
+
+import Cocoa
+
+class TaskMenuItemSeperatorViewController: NSViewController {
+    var name: String? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
