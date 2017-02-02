e3687f1d673810a3ba146a06ad213cfc3c23dd8a
Sinkerine <git@15cm.net>
2017-02-03 00:29:11 +0800

    [+] Init

---
+++ AMM/ServerProfileManager.swift
@@ -0,0 +1,55 @@
+//
+//  AMM.swift
+//  AMM
+//
+//  Created by Sinkerine on 26/01/2017.
+//  Copyright © 2017 sinkerine. All rights reserved.
+//
+
+import Foundation
+
+protocol ServerProfileManagerDelegate: class{
+    func onServerProfilesUpdate(withServerProfiles servers: [ServerProfile])
+}
+
+class ServerProfileManager: NSObject {
+    static let instance: ServerProfileManager = ServerProfileManager()
+    var servers: [ServerProfile] = []
+    weak var delegate: ServerProfileManagerDelegate? = nil
+    
+    fileprivate override init() {
+        super.init()
+        loadPref()
+    }
+    
+    func copyServers() -> [ServerProfile] {
+        return servers.map({server in
+            server.copy() as! ServerProfile
+        })
+    }
+    
+    func savePref() {
+        let defaults = UserDefaults.standard
+        var profiles = [AnyObject]()
+        for server in servers {
+            if server.isValid() {
+                let profile = server.toDictionary()
+                profiles.append(profile as AnyObject)
+            }
+        }
+        defaults.set(profiles, forKey: "serverProfiles")
+        delegate?.onServerProfilesUpdate(withServerProfiles: servers)
+    }
+    
+    func loadPref() {
+        let defaults = UserDefaults.standard
+        servers.removeAll()
+        if let profiles = defaults.array(forKey: "serverProfiles") {
+            for profile in profiles {
+                let server = ServerProfile.fromDictionary(profile as! [String : Any?])
+                servers.append(server)
+            }
+        }
+    }
+    
+}
---
+++ AMM/TaskMenuItemSeperator.swift
@@ -0,0 +1,23 @@
+//
+//  TaskMenuItemSeperator.swift
+//  AMM
+//
+//  Created by Sinkerine on 03/02/2017.
+//  Copyright © 2017 sinkerine. All rights reserved.
+//
+
+import Cocoa
+
+class TaskMenuItemSeperator: NSMenuItem {
+    var viewController: TaskMenuItemSeperatorViewController? = nil
+    init(name: String) {
+        viewController = TaskMenuItemSeperatorViewController(nibName: "TaskMenuItemSeperatorViewController", bundle: nil)
+        viewController?.name = name
+        super.init(title: name, action: nil, keyEquivalent: "")
+        self.view = viewController?.view
+    }
+    
+    required init(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
