a6a2bd0921ac35a54b695fbb5c384bf2c3589148
15cm <sinkerine@gmail.com>
2017-02-16 14:22:37 +0800

    [+] Drag & Drop support to reorder server profiles

--- AMM/Window/PreferencesWindowController.swift
+++ AMM/Window/PreferencesWindowController.swift
@@ -8,9 +8,11 @@
 
 import Cocoa
 
-class PreferencesWindowController: NSWindowController, NSWindowDelegate {
+class PreferencesWindowController: NSWindowController, NSWindowDelegate, NSTableViewDataSource {
     let profileManager = ServerProfileManager.instance
+    let tableViewDragType = "amm.serverprofile"
     
+    @IBOutlet weak var profileTableView: NSTableView!
     @IBOutlet weak var protocolPopUpButton: NSPopUpButton!
     @IBOutlet weak var serverTableScrollView: NSScrollView!
     
@@ -18,7 +20,7 @@ class PreferencesWindowController: NSWindowController, NSWindowDelegate {
         return "PreferencesWindowController"
     }
     
-    @IBOutlet var arrayController: NSArrayController!
+    @IBOutlet var arrayController: ServerProfileArrayController!
     
     override func windowDidLoad() {
         super.windowDidLoad()
@@ -31,6 +33,11 @@ class PreferencesWindowController: NSWindowController, NSWindowDelegate {
             let protocolOfSelection = (arrayController.selection as AnyObject).value(forKey: "protocolRawValue") as! String?
             protocolPopUpButton.selectItem(withTitle: protocolOfSelection!)
         }
+    }
+    
+    override func awakeFromNib() {
+        profileTableView.dataSource = self
+        profileTableView.register(forDraggedTypes: [tableViewDragType])
     }
 
     func reloadProfiles() {
@@ -50,5 +57,39 @@ class PreferencesWindowController: NSWindowController, NSWindowDelegate {
     func windowShouldClose(_ sender: Any) -> Bool {
         reloadProfiles()
         return true
+    }
+    
+    // Drag & Drop to reorder rows
+    func tableView(_ tableView: NSTableView, writeRowsWith rowIndexes: IndexSet, to pboard: NSPasteboard) -> Bool {
+        let data = NSKeyedArchiver.archivedData(withRootObject: rowIndexes)
+        pboard.declareTypes([tableViewDragType], owner: self)
+        pboard.setData(data, forType: tableViewDragType)
+        return true
+    }
+    
+    func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableViewDropOperation) -> NSDragOperation {
+        if dropOperation == .above {
+            return .move
+        }
+        return NSDragOperation()
+    }
+    
+    func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableViewDropOperation) -> Bool {
+        let pbboard = info.draggingPasteboard()
+        if let indexesData = pbboard.data(forType: tableViewDragType) {
+            let oldIndexes: IndexSet = NSKeyedUnarchiver.unarchiveObject(with: indexesData) as! IndexSet
+            var profilesToMove:[ServerProfile] = []
+            var offset = 0
+            for index in oldIndexes {
+                profilesToMove.append(profileManager.servers[index])
+                if index < row {
+                    offset -= 1
+                }
+            }
+            arrayController.remove(atArrangedObjectIndexes: oldIndexes)
+            arrayController.insert(contentsOf: profilesToMove, atArrangedObjectIndexes: IndexSet((row + offset)..<(row + offset + profilesToMove.count)))
+            return true
+        }
+        return false
     }
 }
