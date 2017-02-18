80ce2679531cc6e6765b7ba57e9d168681da0700
Sinkerine <git@15cm.net>
2017-02-18 14:42:25 +0800

    [!] Modify highlight implementation

--- AMM/Main/Constants.swift
+++ AMM/Main/Constants.swift
@@ -7,7 +7,7 @@
 //  Copyright © 2017 sinkerine. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 
 let defaultProtocol = Aria2Protocols.ws
 let defaultHost = "localhost"
@@ -19,4 +19,17 @@ let defaultTaskStatRefreshInterval: Double = 1
 let defaultActiveTaskMaxNum = 5
 let defaultWaitingTaskMaxNum = 5
 let defaultStoppedTaskMaxNum = 5
+
+enum AMMHighLightColors {
+    case text, background
+    
+    func color() -> NSColor {
+        switch self {
+        case .text:
+            return NSColor.white
+        case .background:
+            return NSColor(calibratedRed: 0, green: 128 / 255, blue: 1, alpha: 0.5)
+        }
+    }
+}
 
