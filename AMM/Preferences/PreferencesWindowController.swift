ffcf209ca52d71e44c1bf154cbba63d37b362e42
15cm <sinkerine@gmail.com>
2017-02-19 21:34:30 +0800

    [!] Update [View|Window]Controller for "Preferences" [!] Reorganize files

---
+++ AMM/Preferences/PreferencesWindowController.swift
@@ -0,0 +1,26 @@
+//
+//  PreferencesWindowController.swift
+//  AMM
+//
+//  Created by Sinkerine on 19/02/2017.
+//  Copyright © 2017 sinkerine. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindowController: NSWindowController, NSWindowDelegate {
+    let preferences = AMMPreferences.instance
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    func windowShouldClose(_ sender: Any) -> Bool {
+        preferences.save()
+        preferences.load()
+        return true
+    }
+
+}
