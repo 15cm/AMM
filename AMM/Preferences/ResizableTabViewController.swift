5cb531c8a8b89fbf767d2fe1210b75355026ebbc
15cm <sinkerine@gmail.com>
2017-03-03 22:26:53 +0800

    [+] Make tab views resizable [+] UI for notification settings

---
+++ AMM/Preferences/ResizableTabViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AutoResizeTabViewController.swift
+//  AMM
+//
+//  Created by Sinkerine on 03/03/2017.
+//  Copyright © 2017 sinkerine. All rights reserved.
+//
+
+import Cocoa
+
+class ResizableTabViewController: NSTabViewController {
+    
+    var tabViewSizeDic = [String: NSSize]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        self.tabView.delegate = self
+    }
+    
+    override func tabView(_ tabView: NSTabView, willSelect tabViewItem: NSTabViewItem?) {
+        super.tabView(tabView, willSelect: tabViewItem)
+        if let newItem = tabViewItem {
+            tabViewSizeDic[newItem.label] = tabViewSizeDic[newItem.label] ?? newItem.view?.frame.size
+        }
+    }
+    
+    override func tabView(_ tabView: NSTabView, didSelect tabViewItem: NSTabViewItem?) {
+        super.tabView(tabView, didSelect: tabViewItem)
+        if let newItem = tabViewItem, let window = self.view.window, let contentSize = tabViewSizeDic[newItem.label] {
+            let newWindowSize = window.frameRect(forContentRect: NSRect(origin: CGPoint.zero, size: contentSize)).size
+            
+            var frame = window.frame
+            frame.origin.y = frame.origin.y + (frame.size.height - newWindowSize.height)
+            frame.size = newWindowSize
+            
+            window.setFrame(frame, display: true, animate: true)
+        }
+    }
+}
